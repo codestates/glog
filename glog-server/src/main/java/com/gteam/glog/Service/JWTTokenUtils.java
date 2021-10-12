560470e9a7511eebefec685ad7c3ca41dcc67e49
pastko <storyofstory@naver.com>
2021-10-12 16:04:51 +0900

    feature/login change JWTTokenUtils class, application.properties

--- glog-server/src/main/java/com/gteam/glog/Service/JWTTokenUtils.java
+++ glog-server/src/main/java/com/gteam/glog/Service/JWTTokenUtils.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gteam.glog.Entity.UserInfos;
 import com.gteam.glog.Entity.Users;
 import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -13,9 +14,10 @@ import java.util.Map;
 @Service
 public class JWTTokenUtils {
 
-    private final String SIGN_KEY = "glog-key";
-    private final String PASS_KEY = "";
-    private static final long serialVersionUID = -798416586417070603L;
+    @Value("${JWT.SIGNKEY}")
+    private String SIGN_KEY;
+    @Value("${JWT.SUBJECTKEY}")
+    private String SUBJECT_KEY;
     private static final long JWT_TOKEN_VALIDITY = 10 * 60 * 60;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -36,7 +38,7 @@ public class JWTTokenUtils {
      * @param token -
      * @return
      */
-    private Claims getAllClaimsFromToken(String token) {
+    public Claims getAllClaimsFromToken(String token) {
         return Jwts.parser().setSigningKey(SIGN_KEY).parseClaimsJws(token).getBody();
     }
 
@@ -46,7 +48,7 @@ public class JWTTokenUtils {
      * @param token -
      * @return
      */
-    private Boolean isTokenExpired(String token) {
+    public Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
@@ -58,17 +60,50 @@ public class JWTTokenUtils {
      * 2. HS512알고리즘과 secret key를 가지고 JWT를 서명한다.
      *
      * @param claims -
-     * @param username -
      * @return
      */
-    private String doGenerateToken(Map<String, Object> claims, String username) {
+    private String doGenerateToken(Map<String, Object> claims) {
         return Jwts.builder()
                 .setClaims(claims)
-                .setSubject(username)
+                .setSubject(SUBJECT_KEY)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
                 .signWith(SignatureAlgorithm.HS512, SIGN_KEY)
                 .compact();
+    }
+
+
+    /**
+     * 전달된 OBJECT로 토큰생성
+     *
+     * @param data -
+     * @return
+     */
+    public String generateUsersToken(Object data) {
+        Map<String, Object> claims = objectMapper.convertValue(data, Map.class);
+        return doGenerateToken(claims);
+    }
+
+
+    /**
+     * jwt 토큰에서 유효성 검사를 위한 subject 조회
+     *
+     * @param token -
+     * @return
+     */
+    private String getSubjectFromToken(String token) {
+        return getAllClaimsFromToken(token).getSubject();
+    }
+
+    /**
+     * 토큰 유효성 검증
+     * 만료 기간 및 Subject 검증
+     * @param token -
+     * @return
+     */
+    public Boolean validateUserToken(String token) {
+        final String userId = getSubjectFromToken(token);
+        return (userId.equals(SUBJECT_KEY) && !isTokenExpired(token));
     }
 
     /**
@@ -83,62 +118,5 @@ public class JWTTokenUtils {
     }
     private String extractToken(String authorizationHeader) {
         return authorizationHeader.substring("Bearer ".length());
-    }
-
-
-    /**
-     * users 토큰생성
-     *
-     * @param users
-     * @return
-     */
-    public String generateUsersToken(Users users) {
-        Map<String, Object> claims = objectMapper.convertValue(users, Map.class);
-        return doGenerateToken(claims, users.getUserId());
-    }
-
-    /**
-     * users Info 토큰생성
-     *
-     * @param userInfos -
-     * @return
-     */
-    public String generateUserInfoToken(UserInfos userInfos) {
-        Map<String, Object> claims = objectMapper.convertValue(userInfos, Map.class);
-        return doGenerateToken(claims, userInfos.getUsername());
-    }
-
-    /**
-     * jwt 토큰에서 유효성 검사를 위한 subject 조회
-     *
-     * @param token -
-     * @return
-     */
-    public String getSubjectFromToken(String token) {
-        return getAllClaimsFromToken(token).getSubject();
-    }
-
-    /**
-     * Users 토큰 검증
-     *
-     * @param token -
-     * @param users -
-     * @return
-     */
-    public Boolean validateUserToken(String token, Users users) {
-        final String userId = getSubjectFromToken(token);
-        return (userId.equals(users.getUserId()) && !isTokenExpired(token));
-    }
-
-    /**
-     * Users Info 토큰 검증
-     *
-     * @param token -
-     * @param userInfos -
-     * @return
-     */
-    public Boolean validateUserInfoToken(String token, UserInfos userInfos) {
-        final String username = getSubjectFromToken(token);
-        return (username.equals(userInfos.getUsername()) && !isTokenExpired(token));
     }
 }
