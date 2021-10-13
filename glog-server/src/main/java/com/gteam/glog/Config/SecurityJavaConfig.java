c7223f8058ae6bf6802c64a51e2085964ff0b048
pastko <storyofstory@naver.com>
2021-10-13 17:30:01 +0900

    feature/login change SecurityConfig

--- glog-server/src/main/java/com/gteam/glog/Config/SecurityJavaConfig.java
+++ glog-server/src/main/java/com/gteam/glog/Config/SecurityJavaConfig.java
@@ -1,21 +1,26 @@
 package com.gteam.glog.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
 @EnableWebSecurity
+@Configuration
 public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationManagerBuilder builder;
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.cors().disable()
-                    .csrf().disable()
+        httpSecurity.csrf().disable()
                     .formLogin().disable()
                     .headers().frameOptions().disable();
     }
@@ -24,4 +29,14 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        this.builder = builder;
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        this.builder
+                .authenticationProvider(authenticationProvider);
+    }
+
 }
