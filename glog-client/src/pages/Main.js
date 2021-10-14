00af6c09105fe013d58c505e96a3376feebc5b66
pastko <storyofstory@naver.com>
2021-10-14 12:33:57 +0900

    feature/main add Main.js main.css change app.js

---
+++ glog-client/src/pages/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../css/main.css';
+import Header from './pages/Header';
+
+export default function Main() {
+    return (
+        <>
+            <Header/>
+        </>
+    );
+}
